de3c06c25e624065df548d6c97654fbb83151eeb
Steve Carter <steve.carter@etas.com>
2021-07-20 17:02:25 +0100

    recursive

--- example/demo.py
+++ example/demo.py
@@ -2,7 +2,9 @@ import opml
 
 outline=opml.parse('export.opml')
 
-for element in outline:
-    print(element.text)
-    for subelement in element:
-        print("  " + subelement.text)
+def descend(node, depth=0):
+    print( "  " * depth + "- " + node.text )
+    for subelement in node:
+        descend(subelement, depth+1)
+
+descend(outline[0])
